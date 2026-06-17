67d82b007704d28fd55623cfe5b95e0d66d98ff6
Michael Binford <162678184+binford-development@users.noreply.github.com>
2026-06-16 22:18:11 -0700

    Update TypefaceSkia.cpp

--- Libraries/LibGfx/Font/TypefaceSkia.cpp
+++ Libraries/LibGfx/Font/TypefaceSkia.cpp
@@ -20,6 +20,7 @@
 #include <core/SkTypeface.h>
 #if defined(AK_OS_ANDROID)
 #    include <ports/SkFontMgr_android.h>
+#    include <ports/SkFontScanner_FreeType.h>
 #elif defined(AK_OS_WINDOWS)
 #    include <ports/SkTypeface_win.h>
 #else
@@ -87,7 +88,7 @@ static SkFontMgr& font_manager()
         }
 #endif
 #if defined(AK_OS_ANDROID)
-        font_manager = SkFontMgr_New_Android(nullptr);
+        font_manager = SkFontMgr_New_Android(nullptr, SkFontScanner_Make_FreeType());
 #elif defined(AK_OS_WINDOWS)
         font_manager = SkFontMgr_New_DirectWrite();
 #else
